29a1a3c2be8194a746f2899d5cf4c471d7f868f8
BEKIRKSU <bekirkscu@gmail.com>
2023-09-13 17:25:45 +0100

    methods made

--- main.py
+++ main.py
@@ -7,3 +7,6 @@ for question in question_data:
     question_text = question['text']
     question_answer = question['answer']
     new_question = Question(q_text=question_text, q_answer=question_answer)
+    # You can get rid of the "named" parameters and just have them as positional parameters.
+    question_bank.append(new_question)
+
